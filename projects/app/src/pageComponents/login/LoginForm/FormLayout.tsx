de23cb35e66afddbd11fd850839ec03cb6bdf232
samlindouxue <samlinzhang@example.com>
2025-02-27 17:04:10 +0800

    删除了原来的标签里面的注释，会引起无限循环重定向

--- projects/app/src/pageComponents/login/LoginForm/FormLayout.tsx
+++ projects/app/src/pageComponents/login/LoginForm/FormLayout.tsx
@@ -176,7 +176,7 @@ const FormLayout = ({ children, setPageType, pageType }: Props) => {
             alignItems={'center'}
             justifyContent={'center'}
           >
-            {/* <MyImage src={LOGO_ICON} w={['22.5px', '36px']} alt={'icon'} /> */}
+        
           </Flex>
           <Box ml={[3, 5]} fontSize={['lg', 'xl']} fontWeight={'bold'} color={'myGray.900'}>
             {feConfigs?.systemTitle} 艾克斯科技
