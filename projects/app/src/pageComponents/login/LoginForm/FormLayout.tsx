f206bf3eca8d377ce0cb2b25de9745ce4e127a6f
samlinmac <samlinzhang@qq.com>
2025-03-11 17:16:44 +0800

    首页标题太长,去掉了DeepSeek,并且去掉了原来的logo

--- projects/app/src/pageComponents/login/LoginForm/FormLayout.tsx
+++ projects/app/src/pageComponents/login/LoginForm/FormLayout.tsx
@@ -176,10 +176,10 @@ const FormLayout = ({ children, setPageType, pageType }: Props) => {
             alignItems={'center'}
             justifyContent={'center'}
           >
-            <MyImage src={LOGO_ICON} w={['22.5px', '36px']} alt={'icon'} />
+
           </Flex>
           <Box ml={[3, 5]} fontSize={['lg', 'xl']} fontWeight={'bold'} color={'myGray.900'}>
-            智云DeepSeek-人工智能AI平台
+            智云-人工智能AI平台
           </Box>
         </Flex>
         {!isPc && <I18nLngSelector />}
