fbf530008746e68db5fac4358d3554eb7625d604
samlinmac <samlinzhang@qq.com>
2025-03-11 17:37:30 +0800

    去掉了首页的隐私政策

--- projects/app/src/pages/login/index.tsx
+++ projects/app/src/pages/login/index.tsx
@@ -272,17 +272,6 @@ function CookiesDrawer({ onClose, onAgree }: { onClose: () => void; onAgree: () 
             <Box color={'myGray.900'} fontWeight={'500'} fontSize={'1rem'}>
               {t('login:cookies_tip')}
             </Box>
-            <Box
-              color={'primary.700'}
-              fontWeight={'500'}
-              fontSize={'1rem'}
-              textDecorationLine={'underline'}
-              cursor={'pointer'}
-              w={'fit-content'}
-              onClick={() => window.open(getDocPath('/docs/agreement/privacy/'), '_blank')}
-            >
-              {t('login:privacy_policy')}
-            </Box>
           </Box>
           <Button ml={'0.75rem'} onClick={onAgree}>
             {t('login:agree')}
