182882d1a77128cc6baab046ac6bbb3ea4b9e692
samlinmac <samlinzhang@qq.com>
2025-03-11 17:19:48 +0800

    去掉了首页的服务协议和隐私协议

--- projects/app/src/pageComponents/login/LoginForm/LoginForm.tsx
+++ projects/app/src/pageComponents/login/LoginForm/LoginForm.tsx
@@ -116,23 +116,6 @@ const LoginForm = ({ setPageType, loginSuccess }: Props) => {
             color={'myGray.700'}
             fontWeight={'medium'}
           >
-            {t('login:policy_tip')}
-            <Link
-              ml={1}
-              href={getDocPath('/docs/agreement/terms/')}
-              target={'_blank'}
-              color={'primary.700'}
-            >
-              {t('login:terms')}
-            </Link>
-            <Box mx={1}>&</Box>
-            <Link
-              href={getDocPath('/docs/agreement/privacy/')}
-              target={'_blank'}
-              color={'primary.700'}
-            >
-              {t('login:privacy')}
-            </Link>
           </Flex>
         )}
 
